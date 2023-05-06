2287fbfa86a1073fd59f17faaa8d9b9441ead104
jjortiz016 <jjortiz016@misena.edu.co>
2023-05-05 23:28:37 -0500

    pasar datos a la vista

--- src/main/java/com/introduccion/introspringboot/controllers/IndexController.java
+++ src/main/java/com/introduccion/introspringboot/controllers/IndexController.java
@@ -1,9 +1,13 @@
 package com.introduccion.introspringboot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -13,10 +17,25 @@ public class IndexController {
     //@RequestMapping(value ="/index", method = RequestMethod.GET)
 
     /*@GetMapping(value="/index")*/
-    @GetMapping({"/index","/","home"}) // puede ser uno sola ruta
-    public String index(){
-
+    @GetMapping({"/index","/"}) // puede ser uno sola ruta
+    public String index(Model model){ //tambien se puede utiliza ModelMap
+         model.addAttribute("titulo","Hola Spring framework");
 
         return "index";
     }
+
+    //com Map tambien podemos enviar datos  a la vista
+    /*public String index(Map<String, Object> map){
+        map.put("titulo","Hola Spring framework con map");
+
+        return "index";
+    }*/
+    //ModelAndView tambien podemo enviar datos a la vista
+    @GetMapping("/home")
+    public ModelAndView home(ModelAndView mv){
+        mv.addObject("titulo","Hola spring con ModelAndView");
+        mv.setViewName("index");
+        return mv;
+    }
+
 }
