b509815d221db8255a7ff9ab11bb572ac93e1875
jjortiz016 <jjortiz016@misena.edu.co>
2023-05-15 18:49:19 -0500

    ligeros ajustes

--- src/main/java/com/introduccion/introspringboot/TextosPropertiesConfig.java
+++ src/main/java/com/introduccion/introspringboot/TextosPropertiesConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources({
-      @PropertySource("classpath:textos.properties")
+      @PropertySource("classpath:message.properties")
 })
 public class TextosPropertiesConfig {
 
